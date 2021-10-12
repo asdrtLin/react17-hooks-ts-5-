cb82ca75db99c69856e9786d689741b8d820087f
李士林 <358915905@qq.com>
2021-10-12 19:31:39 +0800

    6-4 路由架构 基础路由系统

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
-import { BrowserRouter , Route  } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import styles from './App.module.css'
 
-import {HomePage} from './pages'
+import { HomePage } from './pages'
 
 function App() {
   return (
     <div className={styles.app}>
       <BrowserRouter>
-      <Route path='/' component={HomePage} />
+        <Switch>
+          <Route path='/' component={HomePage} exact />
+          <Route path='/signIn' render={() => <h1>登录页面</h1>} />
+          <Route  render={() => <h1>404 not found 页面 去火星了</h1>} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
