1bc83567100035bbeeac9a3bfc0f2160af5a5d4d
李士林 <358915905@qq.com>
2021-10-12 19:24:04 +0800

    6-3 初始化路由

---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { Header, Footer, Carousel, SideMenu, ProductCollection } from '../../components'
+import { productList1, productList2, productList3 } from './mockups'
+import sideImage from '../../assets/images/sider_2019_12-09.png'
+import sideImage2 from '../../assets/images/sider_2019_02-04.png'
+import sideImage3 from '../../assets/images/sider_2019_02-04-2.png'
+import { Row, Col, Typography } from 'antd'
+import styles from './HomePage.module.css'
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className={styles['page-content']}>
+                    <Row style={{ marginTop: 20 }}>
+                        <Col span={6}>
+                            <div >
+                                <SideMenu />
+                            </div>
+                        </Col>
+                        <Col span={18}>
+                            <div style={{ background: "blue" }}>
+                                <Carousel />
+                            </div>
+                        </Col>
+                    </Row>
+                    <ProductCollection
+                        title={<Typography.Title level={3} type='warning' >爆款推荐</Typography.Title>}
+                        sideImage={sideImage}
+                        products={productList1}
+                    />
+                    <ProductCollection
+                        title={<Typography.Title level={3} type='danger' >新品上市</Typography.Title>}
+                        sideImage={sideImage2}
+                        products={productList2}
+                    />
+                    <ProductCollection
+                        title={<Typography.Title level={3} type='danger' >国内游推荐</Typography.Title>}
+                        sideImage={sideImage3}
+                        products={productList3}
+                    />
+                </div>
+                <Footer />
+            </div>
+        )
+    }
+}
+export  {HomePage}
