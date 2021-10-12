a1851a7cee35f1c5aa260016190e00c177970b45
李士林 <358915905@qq.com>
2021-10-12 20:56:57 +0800

    7-3 创建State    createState()

---
+++ src/redux/languageReducer.ts
@@ -0,0 +1,16 @@
+interface LanguageState{
+    language : 'en' | 'zh';
+    languageList: {name:string,code:string}[]
+}
+
+const defaultState :LanguageState={
+    language:"zh",
+    languageList:[
+        {name:"中文",code:"zh"},
+        {name:"英文",code:"eh"}
+    ]
+}
+
+export default (state=defaultState,action:any)=>{
+    return state
+}
